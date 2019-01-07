8accbed1be68e46a988ede4146fecec2b3c65447
Yue Du <ifduyue@gmail.com>
2019-01-07 12:30:16 +0800

    Drop support for Python 2.6

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ setup(
     classifiers = [
         'Development Status :: 4 - Beta',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
