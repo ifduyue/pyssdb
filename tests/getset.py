1ebfb7f104aad2d865f5863e9ca113c4c8c867f0
Yue Du <ifduyue@gmail.com>
2019-01-07 14:48:25 +0800

    fix

--- tests/getset.py
+++ tests/getset.py
@@ -2,7 +2,7 @@ from . import TestCase
 
 class TestGetSet(TestCase):
     def test_get(self):
-        self.assertEqual(None, self.get('None')) 
+        self.assertEqual(None, self.ssdb.get('None')) 
 
     def test_set(self):
-        self.assertEqual(1, self.set('set', 'set'))
+        self.assertEqual(1, self.ssdb.set('set', 'set'))
